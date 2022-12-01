7cf93a38bad8dc02c737af8497c710317f9b7bd1
Bruno Jensen <x665.ninja@gmail.com>
2022-12-01 11:52:02 +0100

    feat: day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,49 @@
+import java.util.PriorityQueue
+
+fun PriorityQueue<Int>.offerAndPollIfMaxSize(input: Int, maxSizw: Int) {
+  this.offer(input)
+  if (this.size > maxSizw) {
+    this.poll()
+  }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+  fun part1(input: List<String>): Int {
+    var sum = 0
+    var max = 0
+    for (i in input) {
+      if (i.isBlank()) {
+        sum = 0
+        continue
+      }
+      sum += i.toInt()
+      max = Math.max(sum, max)
     }
+    return max
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+  fun part2(input: List<String>): Int {
+    val queue: PriorityQueue<Int> = PriorityQueue()
+    var sum = 0
+    for (i in input) {
+      if (i.isBlank()) {
+        queue.offerAndPollIfMaxSize(sum, 3)
+        sum = 0
+        continue
+      }
+      sum += i.toInt()
     }
+    queue.offerAndPollIfMaxSize(sum, 3)
+    return queue.sum()
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01.test")
+  check(part1(testInput) == 24000)
+  check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput("Day01")
+  println(part1(input))
+  println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,8 +5,15 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("resources", "$name.txt")
     .readLines()
+
+/**
+* Reads lines from the given input txt file.
+*/
+fun readInputAsInts(name: String) = File("resources", "$name.txt")
+    .readLines()
+    .map { it.toInt() }
 
 /**
  * Converts string to md5 hash.
